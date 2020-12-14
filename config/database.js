b4741f2b83689e984039cc55a12d2f627bf8dd47
jatinder <jatindermashall@gmail.com>
2020-12-14 22:32:59 +0530

    database.js

--- config/database.js
+++ config/database.js
@@ -5,11 +5,11 @@ module.exports = ({ env }) => ({
       connector: 'bookshelf',
       settings: {
         client: 'mysql',
-        host: env('DATABASE_HOST', 'localhost'),
+        host: env('DATABASE_HOST', 'durvbryvdw2sjcm5.cbetxkdyhwsb.us-east-1.rds.amazonaws.com'),
         port: env.int('DATABASE_PORT', 3306),
-        database: env('DATABASE_NAME', 'childname'),
-        username: env('DATABASE_USERNAME', 'root'),
-        password: env('DATABASE_PASSWORD', ''),
+        database: env('DATABASE_NAME', 'khsbh3zpl8yhy9d2'),
+        username: env('DATABASE_USERNAME', 'nr5thw02e30x0ilt'),
+        password: env('DATABASE_PASSWORD', 'zw5pksihqrgujgnl'),
         ssl: env.bool('DATABASE_SSL', false),
       },
       options: {}
